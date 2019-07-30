5471225461abd124896c6bd624dd25cd5b543dd7
mhelwick54 <mhelwick54@gmail.com>
2019-07-30 14:27:27 -0500

    Delete main.d

--- bin/main.d
+++
@@ -1,4 +0,0 @@
-bin/main.o: src/main.c include/memory_layout.h include/registers.h \
- include/memory_layout.h include/sprites.h include/display.h \
- include/registers.h include/sprites.h include/window.h \
- include/info_wins.h include/display.h include/info_wins.h
